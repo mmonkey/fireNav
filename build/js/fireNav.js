1f24f22d82c73b46e1ec23a32ea36dfd9f51d21d
unknown <mmonkey@gmail.com>
2015-01-05 09:12:27 -0600

    added style to build index.html

--- build/js/fireNav.js
+++ build/js/fireNav.js
@@ -4,20 +4,26 @@
 	function fireNav(options) {
 
 		this.defaults = {
-			selector: "#fireNav",
+			container: "#fireNav",
 			type: "Off-Canvas"
 		};
-		
+
+		// Merge defaults into options
 		this.options = options || {};
-		
 		for (var opt in this.defaults) {
 			if (this.defaults.hasOwnProperty(opt) && !this.options.hasOwnProperty(opt)) {
 				this.options[opt] = this.defaults[opt];
 			}
 		}
 
+		// Set up Velocity
+		var V;
+		if(window.jQuery) { V = $.Velocity; } else { V = Velocity; }
+
+		this.nav = document.querySelectorAll(this.options.container)[0];
+
 		this.init = function init() {
-			console.log(this.options.selector);
+			console.log(this.options.container);
 		}
 	}
 
--- dist/fireNav.js
+++ dist/fireNav.js
@@ -1,14 +1,18 @@
 /* fireNav (0.1.0). (C) 2014 CJ O'Hara amd Tyler Fowle. MIT @license: en.wikipedia.org/wiki/MIT_License */
 !function(t) {
-    function s(t) {
+    function i(i) {
         this.defaults = {
-            selector: "#fireNav",
+            container: "#fireNav",
             type: "Off-Canvas"
-        }, this.options = t || {};
-        for (var s in this.defaults) this.defaults.hasOwnProperty(s) && !this.options.hasOwnProperty(s) && (this.options[s] = this.defaults[s]);
+        }, // Merge defaults into options
+        this.options = i || {};
+        for (var o in this.defaults) this.defaults.hasOwnProperty(o) && !this.options.hasOwnProperty(o) && (this.options[o] = this.defaults[o]);
+        // Set up Velocity
+        var n;
+        n = t.jQuery ? $.Velocity : Velocity, this.nav = document.querySelectorAll(this.options.container)[0], 
         this.init = function() {
-            console.log(this.options.selector);
+            console.log(this.options.container);
         };
     }
-    t.fireNav = s;
+    t.fireNav = i;
 }(window);
