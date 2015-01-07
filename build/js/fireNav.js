b9f4bdb428e0568885c1b05a56639e1c18d73375
unknown <mmonkey@gmail.com>
2015-01-07 02:07:06 -0600

    added jump nav function

--- build/js/fireNav.js
+++ build/js/fireNav.js
@@ -1,32 +1,175 @@
-/* fireNav (0.1.0). (C) 2014 CJ O'Hara amd Tyler Fowle. MIT @license: en.wikipedia.org/wiki/MIT_License */
-(function ( window, undefined ) {
+/* fireNav (0.1.0). (C) 2014 CJ O'Hara and Tyler Fowle. MIT @license: en.wikipedia.org/wiki/MIT_License */
+(function (window, document) {
 
-	function fireNav(options) {
+	FireNav = function() {
+	};
 
-		this.defaults = {
-			container: "#fireNav",
-			type: "Off-Canvas"
-		};
+	// Set up Velocity
+	var V = (window.jQuery) ? $.Velocity : Velocity;
 
-		// Merge defaults into options
-		this.options = options || {};
-		for (var opt in this.defaults) {
-			if (this.defaults.hasOwnProperty(opt) && !this.options.hasOwnProperty(opt)) {
-				this.options[opt] = this.defaults[opt];
+	// Extend defaults into opts, returns options
+	function extend(opts, defaults) {
+		var options = opts || {};
+		for (var opt in defaults) {
+			if (defaults.hasOwnProperty(opt) && !options.hasOwnProperty(opt)) {
+				options[opt] = defaults[opt];
 			}
 		}
+		return options;
+	}
 
-		// Set up Velocity
-		var V;
-		if(window.jQuery) { V = $.Velocity; } else { V = Velocity; }
-
-		this.nav = document.querySelectorAll(this.options.container)[0];
-
-		this.init = function init() {
-			console.log(this.options.container);
+	// Creates an event listener
+	function listen(elem, type, eventHandle) {
+		if (elem === null || typeof(elem) == 'undefined') return;
+		if (elem.addEventListener) {
+			elem.addEventListener(type, eventHandle, false);
+		} else if (elem.attachEven ) {
+			elem.attachEvent("on" + type, eventHandle);
+		} else {
+			elem["on"+type]=eventHandle;
 		}
 	}
 
-	window.fireNav = fireNav;
+	// Add class to node's classList
+	function addClass(node, newClass) {
+		if (node.classList) {
+				node.classList.add(newClass);
+		} else {
+				node.className += ' ' + newClass;
+		}
+	}
 
-})( window );
+	// Remove class from node's classList
+	function removeClass(node, rmClass) {
+		if (node.classList) {
+				node.classList.remove(rmClass);
+		} else {
+			node.className = node.className.replace(new RegExp('(^|\\b)' + rmClass.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
+		}
+	}
+
+	// Returns true if node has className
+	function hasClass(node, className) {
+		var result = false;
+		if (node.classList) {
+			if(node.classList.contains(className)) {
+				result = true;
+			}
+		}
+		return result;
+	}
+
+	/**
+	 * FireNav.jump function
+	 * Adds a smart jump menu that appends to the body
+	 */
+	FireNav.jump = function(opts) {
+		var defaults = {
+			sectionClass: ".section",
+			position: "right",
+			speed: 800
+		};
+
+		var options = extend(opts, defaults);
+		var sections = document.querySelectorAll(options.sectionClass);
+		var nav;
+
+		function constructNav() {
+			nav = document.createElement('NAV');
+			nav.id = 'jumpNav';
+
+			if(sections.length > 0) {
+				for(var i = 0; i < sections.length; i++) {
+					var li = document.createElement('LI');
+					var a = document.createElement('A');
+					a.href = '#' + sections[i].id;
+					a.innerText = sections[i].dataset.jumpName;
+					li.dataset.jumpClass = sections[i].dataset.jumpClass;
+					li.appendChild(a);
+					nav.appendChild(li);
+				}
+			}
+			document.body.appendChild(nav);
+		}
+
+		function getZoomAmount() {
+			var result = 1;
+			if (document.body.getBoundingClientRect) {
+				var rect = document.body.getBoundingClientRect();
+				var physicalW = rect.right - rect.left;
+				var logicalW = document.body.offsetWidth;
+				result = (physicalW / logicalW);
+			}
+			return result;
+		}
+
+		function getWindowScrollTop() {
+			var result = 0;
+			if ('pageXOffset' in window) {
+				result = window.pageYOffset;
+			} else {
+				result = document.documentElement.scrollTop / getZoomAmount();
+			}
+			return result;
+		}
+
+		function getWindowScrollLeft() {
+			var result = 0;
+			if ('pageXOffset' in window) {
+				result =  window.pageXOffset;
+			} else {
+				result = document.documentElement.scrollLeft / getZoomAmount();
+			}
+			return result;
+		}
+
+		function getScrollTop(node) {
+			return node.getBoundingClientRect().top;
+		}
+
+		function getScrollLeft(node) {
+			return node.getBoundingClientRect().left;
+		}
+
+		function init() {
+			constructNav();
+
+			// Create event listens on the jumpnav
+			for(var i = 0; i < nav.getElementsByTagName('a').length; i++) {
+				listen(nav.getElementsByTagName('a')[i], 'click', function(e) {
+					e.preventDefault();
+					V(document.querySelector(e.target.hash), "scroll", {duration: options.speed, easing: "ease-in-out"});
+				});
+			}
+		}
+
+		init();
+
+		// Returns the active hash node based on nodes scrollTop
+		function getActiveHashNode(hashNodes) {
+			var result = null;
+			var max = -1;
+			for(var i = 0; i < hashNodes.length; i++) {
+				var elemY = getScrollTop(hashNodes[i]);
+				if(elemY <= 0) {
+					if(elemY > max || max === -1) {
+						max = elemY;
+						result = hashNodes[i];
+					}
+				}
+			}
+			return result;
+		}
+
+		listen(window, 'scroll', function(e) {
+			var activeHashNode = getActiveHashNode(sections);
+			window.location.hash = activeHashNode.id;
+		});
+
+		listen(window, 'resize', function(e) {
+		});
+	};
+
+	window.FireNav = FireNav;
+
+})(window, document);
--- dist/fireNav.js
+++ dist/fireNav.js
@@ -1,18 +1,60 @@
-/* fireNav (0.1.0). (C) 2014 CJ O'Hara amd Tyler Fowle. MIT @license: en.wikipedia.org/wiki/MIT_License */
-!function(t) {
-    function i(i) {
-        this.defaults = {
-            container: "#fireNav",
-            type: "Off-Canvas"
-        }, // Merge defaults into options
-        this.options = i || {};
-        for (var o in this.defaults) this.defaults.hasOwnProperty(o) && !this.options.hasOwnProperty(o) && (this.options[o] = this.defaults[o]);
-        // Set up Velocity
-        var n;
-        n = t.jQuery ? $.Velocity : Velocity, this.nav = document.querySelectorAll(this.options.container)[0], 
-        this.init = function() {
-            console.log(this.options.container);
-        };
+/* fireNav (0.1.0). (C) 2014 CJ O'Hara and Tyler Fowle. MIT @license: en.wikipedia.org/wiki/MIT_License */
+!function(e, n) {
+    // Extend defaults into opts, returns options
+    function t(e, n) {
+        var t = e || {};
+        for (var a in n) n.hasOwnProperty(a) && !t.hasOwnProperty(a) && (t[a] = n[a]);
+        return t;
     }
-    t.fireNav = i;
-}(window);
+    // Creates an event listener
+    function a(e, n, t) {
+        null !== e && "undefined" != typeof e && (e.addEventListener ? e.addEventListener(n, t, !1) : e.attachEven ? e.attachEvent("on" + n, t) : e["on" + n] = t);
+    }
+    FireNav = function() {};
+    // Set up Velocity
+    var r = e.jQuery ? $.Velocity : Velocity;
+    /**
+	 * FireNav.jump function
+	 * Adds a smart jump menu that appends to the body
+	 */
+    FireNav.jump = function(i) {
+        function o() {
+            if (s = n.createElement("NAV"), s.id = "jumpNav", v.length > 0) for (var e = 0; e < v.length; e++) {
+                var t = n.createElement("LI"), a = n.createElement("A");
+                a.href = "#" + v[e].id, a.innerText = v[e].dataset.jumpName, t.dataset.jumpClass = v[e].dataset.jumpClass, 
+                t.appendChild(a), s.appendChild(t);
+            }
+            n.body.appendChild(s);
+        }
+        function l(e) {
+            return e.getBoundingClientRect().top;
+        }
+        function c() {
+            o();
+            // Create event listens on the jumpnav
+            for (var e = 0; e < s.getElementsByTagName("a").length; e++) a(s.getElementsByTagName("a")[e], "click", function(e) {
+                e.preventDefault(), r(n.querySelector(e.target.hash), "scroll", {
+                    duration: f.speed,
+                    easing: "ease-in-out"
+                });
+            });
+        }
+        // Returns the active hash node based on nodes scrollTop
+        function u(e) {
+            for (var n = null, t = -1, a = 0; a < e.length; a++) {
+                var r = l(e[a]);
+                0 >= r && (r > t || -1 === t) && (t = r, n = e[a]);
+            }
+            return n;
+        }
+        var s, d = {
+            sectionClass: ".section",
+            position: "right",
+            speed: 800
+        }, f = t(i, d), v = n.querySelectorAll(f.sectionClass);
+        c(), a(e, "scroll", function() {
+            var n = u(v);
+            e.location.hash = n.id;
+        }), a(e, "resize", function() {});
+    }, e.FireNav = FireNav;
+}(window, document);
